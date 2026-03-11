9325572041635f272bc71458fe92a7f54faf2338
Brian Cefali <brocef@users.noreply.github.com>
2026-03-11 16:19:48 -0400

    feat: add interfaces barrel export

---
+++ src/lib/core/interfaces/index.ts
@@ -0,0 +1,24 @@
+export type {
+    TDisplayable,
+    TChecksummable,
+} from "./shared.interfaces.js"
+
+export type {
+    TPremiseCrud,
+    TVariableManagement,
+    TArgumentExpressionQueries,
+    TArgumentRoleState,
+    TArgumentEvaluation,
+    TArgumentLifecycle,
+    TArgumentIdentity,
+} from "./argument-engine.interfaces.js"
+
+export type {
+    TExpressionMutations,
+    TExpressionQueries,
+    TVariableReferences,
+    TPremiseClassification,
+    TPremiseEvaluation,
+    TPremiseLifecycle,
+    TPremiseIdentity,
+} from "./premise-engine.interfaces.js"
