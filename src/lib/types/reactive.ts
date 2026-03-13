86e5e66f81fadd938c03d95d62dab7bc345b4a1e
Brian Cefali <brocef@users.noreply.github.com>
2026-03-13 14:25:01 -0400

    feat: remove TSource and sources from TReactiveSnapshot

--- src/lib/types/reactive.ts
+++ src/lib/types/reactive.ts
@@ -3,7 +3,6 @@ import type {
     TCorePremise,
     TCorePropositionalExpression,
     TCorePropositionalVariable,
-    TCoreSource,
     TCoreVariableSourceAssociation,
     TCoreExpressionSourceAssociation,
 } from "../schemata/index.js"
@@ -23,13 +22,11 @@ export type TReactiveSnapshot<
     TPremise extends TCorePremise = TCorePremise,
     TExpr extends TCorePropositionalExpression = TCorePropositionalExpression,
     TVar extends TCorePropositionalVariable = TCorePropositionalVariable,
-    TSource extends TCoreSource = TCoreSource,
 > = {
     argument: TArg
     variables: Record<string, TVar>
     premises: Record<string, TReactivePremiseSnapshot<TPremise, TExpr>>
     roles: TCoreArgumentRoleState
-    sources: Record<string, TSource>
     variableSourceAssociations: Record<string, TCoreVariableSourceAssociation>
     expressionSourceAssociations: Record<
         string,
